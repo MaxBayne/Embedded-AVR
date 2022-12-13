d58931801f905f9615c51aa19636eca7733e7f63
Eng.Mohammed Salah <maxbayne@gmail.com>
2022-12-13 22:06:53 +0200

    fix the timer2 issue

--- Projects/AVRTutorials/src/main.cpp
+++ Projects/AVRTutorials/src/main.cpp
@@ -36,7 +36,7 @@ void toggle_Pin_A2()
     //We Can Stop Timer Here by set Clock Source = nothing
 
 
-   // _log.WriteLine("1 Second Fired , and Toggle Pin A2");
+   //_log.WriteLine("1 Second Fired , and Toggle Pin A2 \n");
 }
 
 int main(void)
@@ -47,36 +47,13 @@ int main(void)
 
   Timer timer0 = Timer(TIMER_0,F_CPU);
   //Timer timer1 = Timer(TIMER_1,F_CPU);
-  //Timer timer2 = Timer(TIMER_2,F_CPU);
+  Timer timer2 = Timer(TIMER_2,F_CPU);
 
   GPIO::Port_Direction(GPIO_PORT_A,GPIO_DIRECTION_OUTPUT);
   
-timer0.Start(1,UNIT_SECOND,toggle_Pin_A0);
-//timer1.Start(1,UNIT_SECOND,toggle_Pin_A1);
-//timer2.Start(1,UNIT_SECOND,toggle_Pin_A2);
-
-
-  //Set Timer Initial Value = 0
-  //TCNT0=0;
-  //TCNT2=0;
-
-  //Set Timer Mode To Normal and Prescaler 1024
-  //TCCR0=0b00000101;
-  //TCCR2=0b00000101;
-
-  //while ((TIFR&(1<<TOV0))==0);
-
-  //Enable Global Interrupt using register (SREG)
-  //BITWISE_SET_BIT(SREG,SREG_I);
-  //sei();
-  
-  //Enable Timer0 Interrupt using register (TIMSK)
-  //BITWISE_SET_BIT(TIMSK,TOIE0);
-  //BITWISE_SET_BIT(TIMSK,TOIE2);
-
-  
-
-
+  timer0.Start(1,UNIT_SECOND,toggle_Pin_A0);
+  //timer1.Start(1,UNIT_SECOND,toggle_Pin_A1);
+  timer2.Start(1,UNIT_SECOND,toggle_Pin_A2);
 
 
   //Loop ----------------------------------------------------
@@ -88,34 +65,9 @@ timer0.Start(1,UNIT_SECOND,toggle_Pin_A0);
 
     GPIO::Pin_Set(GPIO_IO_PA1);
 
-	//Timer* timer2 = TimerInterrupt::Timer2;
-
     delay_ms(1000);
-
-	
-	//_log.WriteLine("Timer2 GetOverflowInteger = \n");
-	//_log.WriteFloat(timer2->GetOverflowInteger());
-
 
   }
 
 }
 
-
-/*
-int timer2Counter;
-ISR(TIMER2_OVF_vect)
-{
-	timer2Counter++;
-
-	if(timer2Counter==61)
-	{
-		GPIO::Pin_Toggle(GPIO_IO_PA2);
-
-		_log.WriteLine("Timer2 Tick Second \n");
-
-		timer2Counter=0;
-	}
-	
-}
-*/
